d6c4d835ab6225b549cd815079b5dc21122e903f
Larkin <larkiningram@gmail.com>
2019-09-10 16:04:15 -0500

    got buttons working and point totals working but not wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,62 @@
 
+var wins = 0;
+var losses = 0;
 var score = 0;
+var target = Math.floor(Math.random() * 200 + 1);
 var red = Math.floor(Math.random() * 9 + 1);
 var blue = Math.floor(Math.random() * 9 + 1);
 var yellow = Math.floor(Math.random() * 9 + 1);
 var green = Math.floor(Math.random() * 9 + 1);
 
+function restart() {
+    score = 0;
+    target = Math.floor(Math.random() * 200 + 1);
+    red = Math.floor(Math.random() * 9 + 1);
+    blue = Math.floor(Math.random() * 9 + 1);
+    yellow = Math.floor(Math.random() * 9 + 1);
+    green = Math.floor(Math.random() * 9 + 1);
+};
 
-$(".red-crystal").on("click", function(){
+$(".tar-score").html(target);
+
+$(".red-crystal").on("click", function () {
     score += red;
+    console.log(score);
+    console.log("red");
+    $(".u-score").html(score);
 });
 
-$(".blue-crystal").on("click", function(){
+$(".blue-crystal").on("click", function () {
     score += blue;
+    console.log(score);
+    console.log("blue");
+    $(".u-score").html(score);
 });
 
-$(".yellow-crystal").on("click", function(){
+$(".yellow-crystal").on("click", function () {
     score += yellow;
+    console.log(score);
+    console.log("yellow");
+    $(".u-score").html(score);
 });
 
-$(".green-crystal").on("click", function(){
+$(".green-crystal").on("click", function () {
     score += green;
+    console.log(score);
+    console.log("green");
+    $(".u-score").html(score);
 });
+
+
+if (score === target) {
+    wins++;
+    $(".wins").html("Wins: " + wins);
+    restart();
+}
+else if (score > target) {
+    losses++;
+    $(".losses").html("Losses: " + losses);
+    restart();
+}
+
+// $(".u-score").html(score);
